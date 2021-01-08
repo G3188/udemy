2ef4b1fe976959feb7d8e8affd382d2601c91be6
Garba Soumounou <garbasoumounou365@gmail.com>
2021-01-08 19:17:45 +0000

    Solution à la septième partie du tp

--- src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -2,6 +2,7 @@ package com.mycompany.dvdstore.repository;
 
 import com.mycompany.dvdstore.entity.Movie;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,18 +10,27 @@ import java.util.List;
 
 public class FileMovieRepository implements MovieRepositoryInterface {
     private List<Movie> movieList = new ArrayList<>();
+    File file = null;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
 
     public void addMovie(Movie movie){
         FileWriter writer;
         try{
-            writer=new FileWriter("C:\\temp\\movies.txt",true);
+            writer=new FileWriter(file ,true);
             writer.write(movie.getTitle()+";"+movie.getGenre()+"\n");
             writer.close();
         }
         catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println("The movie "+movie.getTitle()+" has been added " +"in movies.txt");
+        System.out.println("The movie "+movie.getTitle()+" has been added " + file);
     }
 
 }
